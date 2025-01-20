6146fa367c6f15fcb11fdac86a87e452ce2aa4da
Xufei Cao <ffeix06@gmail.com>
2025-01-20 11:04:20 +0000

    Refactored
    Refactored the Euclidean Algorithm

--- original code.py
+++ original code.py
@@ -13,19 +13,34 @@ class EuclideanAlgorithm:
         self.b=b
 #calculate gcd   
     def calculate_gcd(self):
-        a,b=self.a,self.b
 #start a while loop when b doesn’t equals to 0        
-        while b!=0:
+        while self.b!=0:
 #replace a by the remainder of a%b
-            a=a%b
-#exchange the values               
-            a,b=b,a
+           self.a,self.b=self.b,self.a%self.b
 #return gcd            
-        return a
-    
-num1=int(input("please enter the first number"))
-num2=int(input("please enter the second number"))
-get_gcd=EuclideanAlgorithm(num1,num2)
-#calling the calculate_gcd method
-result=get_gcd.calculate_gcd()
-print(result)
+        return self.a
+#check if the input a positive integer    
+    def get_number(self):
+#if inputs are not integers,return False
+       if not isinstance(self.a,int):
+           return False
+       if not isinstance(self.b,int):
+           return False 
+#if inputs are negative or equals to 0, return False
+       if self.a<=0 or self.b<=0:
+           return False
+#if inputs are positive integers, return True
+       else:
+           return True
+#ask for inputs       
+try:
+    num1=int(input("please enter the first number"))
+    num2=int(input("please enter the second number"))
+    get_gcd=EuclideanAlgorithm(num1,num2)
+#check if the inputs valid
+    if get_gcd.get_number() == False:
+       print("please enter a positive integer")
+    else:
+       print(f"gcd is {get_gcd.calculate_gcd()}")
+except ValueError:
+    print("invalid input,please enter a pisitive integer")
